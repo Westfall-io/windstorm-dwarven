9259ab56d4b52540e947b3b94cb783f3b1f56220
Christopher Cox <christophercox@MacBook-Pro-5.local>
2024-11-01 15:24:26 -0400

    :passport_control: Adding authentication to FastAPI

--- src/main.py
+++ src/main.py
@@ -25,17 +25,50 @@ PGUSER = os.environ.get("PGUSER","postgres")
 PGPASSWD = os.environ.get("PGPASSWD","mysecretpassword")
 PGDBNAME = os.environ.get("PGDBNAME","pgdb")
 
+KCREALM = os.environ.get("KCREALM","test")
+KCADDR = os.environ.get("KCADDR","https://keycloak.digitalforge.app")
+
 import json
 import math
 from uuid import uuid4 as uuid_gen
 
-from typing import Union
+from typing import Union, Annotated
 from datetime import datetime
 
 import sqlalchemy as db
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import OAuth2AuthorizationCodeBearer
+
+from jwt import PyJWKClient
+import jwt
+
+oauth_2_scheme = OAuth2AuthorizationCodeBearer(
+    tokenUrl=KCADDR+"/"+KCREALM+"/protocol/openid-connect/token",
+    authorizationUrl=KCADDR+"/"+KCREALM+"/protocol/openid-connect/auth",
+    refreshUrl=KCADDR+"/"+KCREALM+"/protocol/openid-connect/token",
+)
+
+async def valid_access_token(
+    access_token: Annotated[str, Depends(oauth_2_scheme)]
+):
+    url = KCADDR+"/"+KCREALM+"/protocol/openid-connect/certs"
+    optional_custom_headers = {"User-agent": "custom-user-agent"}
+    jwks_client = PyJWKClient(url, headers=optional_custom_headers)
+
+    try:
+        signing_key = jwks_client.get_signing_key_from_jwt(access_token)
+        data = jwt.decode(
+            access_token,
+            signing_key.key,
+            algorithms=["RS256"],
+            audience="api",
+            options={"verify_exp": True},
+        )
+        return data
+    except jwt.exceptions.InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Not authenticated")
 
 origins = [
     "https://windstorm-api.westfall.io",
@@ -461,7 +494,7 @@ def main():
 
         return output
 
-    @app.put("/models/verifications/{verification_id}", tags=["models"])
+    @app.put("/models/verifications/{verification_id}", tags=["models"], dependencies=[Depends(valid_access_token)])
     def verify(verification_id: int, verify: bool):
         verification = session \
             .query(Verifications) \
@@ -1643,7 +1676,7 @@ def main():
     class Status(BaseModel):
         status: str
 
-    @app.put("/auth/update_thread/{thread_id}", tags=["auth"])
+    @app.put("/auth/update_thread/{thread_id}", tags=["auth"], dependencies=[Depends(valid_access_token)])
     def update_te(thread_id: int, status: Status):
         if status.status == 'windrunner_1':
             # No input files
@@ -1683,7 +1716,7 @@ def main():
         else:
             return {'error': 'Not found'}
 
-    @app.put("/auth/add_thread/{action_id}", tags=["auth"])
+    @app.put("/auth/add_thread/{action_id}", tags=["auth"], dependencies=[Depends(valid_access_token)])
     def auth_add_thread(action_id: int):
         action = session \
             .query(Actions) \
