f4b8a9fef4f8ba4eb61fe4339af52138884fe463
Christopher Cox <christophercox@MacBook-Pro-5.local>
2024-11-01 17:59:34 -0400

    :zap: Adding oauth

--- src/sampleapp.py
+++ src/sampleapp.py
@@ -2,6 +2,36 @@ from typing import Union
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import OAuth2AuthorizationCodeBearer
+
+from jwt import PyJWKClient
+import jwt
+
+oauth_2_scheme = OAuth2AuthorizationCodeBearer(
+    tokenUrl=KCADDR+"/"+KCREALM+"/protocol/openid-connect/token",
+    authorizationUrl=KCADDR+"/"+KCREALM+"/protocol/openid-connect/auth",
+    refreshUrl=KCADDR+"/"+KCREALM+"/protocol/openid-connect/token",
+)
+
+async def valid_access_token(
+    access_token: Annotated[str, Depends(oauth_2_scheme)]
+):
+    url = KCADDR+"/"+KCREALM+"/protocol/openid-connect/certs"
+    optional_custom_headers = {"User-agent": "custom-user-agent"}
+    jwks_client = PyJWKClient(url, headers=optional_custom_headers)
+
+    try:
+        signing_key = jwks_client.get_signing_key_from_jwt(access_token)
+        data = jwt.decode(
+            access_token,
+            signing_key.key,
+            algorithms=["RS256"],
+            audience="api",
+            options={"verify_exp": True},
+        )
+        return data
+    except jwt.exceptions.InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Not authenticated")
 
 def main():
     tags_metadata = [
