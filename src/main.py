7d8bb9e66d14d1aeaf3666c7a27c3dbf20c68cb1
Chris Cox <chris.cox@westfall.io>
2024-12-03 22:17:40 -0500

    :bug: Fixed an issue with correct audience

--- src/main.py
+++ src/main.py
@@ -45,18 +45,19 @@ oauth_2_scheme = OAuth2AuthorizationCodeBearer(
 async def valid_access_token(
     access_token: Annotated[str, Depends(oauth_2_scheme)]
 ):
-    logger.warning(access_token)
+    logger.warning('Checking access token: {}'.format(access_token))
     url = KCADDR+"/realms/"+KCREALM+"/protocol/openid-connect/certs"
     optional_custom_headers = {"User-agent": "custom-user-agent"}
     jwks_client = PyJWKClient(url, headers=optional_custom_headers)
 
     try:
         signing_key = jwks_client.get_signing_key_from_jwt(access_token)
+        logger.warning('Checking signing key: {}'.format(signing_key))
         data = jwt.decode(
             access_token,
             signing_key.key,
             algorithms=["RS256"],
-            audience="api",
+            audience="account",
             options={"verify_exp": True},
         )
         return data
